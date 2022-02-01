2e10a65971a8421040e612e43b8fe6e7a50c74e9
Saimon Davi <saimon.dsfg@gmail.com>
2022-02-01 00:02:34 -0300

    Exemplo da função reduce

--- assets/test.js
+++ assets/test.js
@@ -1,17 +1,38 @@
-// function filtraPares(arr) {
-//     return arr.filter(callbackFuncao);
-// }
+function somaNumeros(arr) {
+    return arr.reduce(function(prev, current){
+        // console.log({ prev });
+        // console.log({ current });
+        return prev + current;
+    }, 0);
+}
 
-// function callbackFuncao(item) {
-//     console.log(item % 2 === 0);
-//     return item % 2 === 0;
-// }
+const nums = [1, 2, 3];
 
-const filtraPares = (arr) => arr.filter((item) => {
-    console.log(item % 2 === 0);
-    return item % 2 === 0;
-}) 
+console.log(somaNumeros(nums))
 
-const nums = [1, 2, 3, 4, 5]
+///////////////////////////////////////////////////
 
-console.log(filtraPares(nums))
+const lista = [
+    {
+        nome: 'item 1',
+        preco: 20
+    },
+    {
+        nome: 'item 2',
+        preco: 20
+    },
+    {
+        nome: 'item 3',
+        preco: 20
+    },  
+];
+
+const saldoDisponivel = 100;
+
+function calculaSaldo(saldoDisponivel, lista) {
+    return lista.reduce(function(prev, current){
+        return prev - current.preco;
+    }, saldoDisponivel)
+}
+
+console.log(calculaSaldo(saldoDisponivel,lista))
