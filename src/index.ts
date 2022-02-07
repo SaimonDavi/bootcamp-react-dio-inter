c17fc051f27739412b52f3236e9e55b2d0de73ac
Saimon Davi <saimon.dsfg@gmail.com>
2022-02-07 12:36:23 -0300

    Adicionando tratamento de erros simples

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import errorHandler from "./middlewares/error-handler.middleware";
 import statusRoute from "./routes/status.route";
 import usersRoute from "./routes/users.route";
 
@@ -8,8 +9,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(usersRoute);
-
 app.use(statusRoute);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
     console.log("Aplicação executando na porta 3000");
